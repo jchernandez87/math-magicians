1cd71d1a53ff73ae4879fb30940c91c06c170979
unknown <jchernandez827@gmail.com>
2021-08-12 18:23:01 -0500

    Add calculator functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
+class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.target = e.target;
+    this.result = calculate(this.state, this.target.name);
+    this.setState(this.result);
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="mainContainer">
         <div className="gridContainer">
-          <div className="result">0</div>
-          <div className="itemA">AC</div>
-          <div className="itemB">+/-</div>
-          <div className="itemC">%</div>
-          <div className="itemD">÷</div>
-          <div className="itemE">7</div>
-          <div className="itemF">8</div>
-          <div className="itemG">9</div>
-          <div className="itemH">×</div>
-          <div className="itemI">4</div>
-          <div className="itemJ">5</div>
-          <div className="itemK">6</div>
-          <div className="itemL">-</div>
-          <div className="itemM">1</div>
-          <div className="itemN">2</div>
-          <div className="itemO">3</div>
-          <div className="itemP">+</div>
-          <div className="itemQ">0</div>
-          <div className="itemR">.</div>
-          <div className="itemS">=</div>
+          <div className="result">{`${total === null ? '' : total} ${operation === null ? '' : operation} ${next === null ? '' : next} ` }</div>
+          <button onClick={this.handleClick} type="button" name="AC" className="itemA">AC</button>
+          <button onClick={this.handleClick} type="button" name="+/-" className="itemB">+/-</button>
+          <button onClick={this.handleClick} type="button" name="%" className="itemC">%</button>
+          <button onClick={this.handleClick} type="button" name="÷" className="itemD">÷</button>
+          <button onClick={this.handleClick} type="button" name="7" className="itemE">7</button>
+          <button onClick={this.handleClick} type="button" name="8" className="itemF">8</button>
+          <button onClick={this.handleClick} type="button" name="9" className="itemG">9</button>
+          <button onClick={this.handleClick} type="button" name="x" className="itemH">×</button>
+          <button onClick={this.handleClick} type="button" name="4" className="itemI">4</button>
+          <button onClick={this.handleClick} type="button" name="5" className="itemJ">5</button>
+          <button onClick={this.handleClick} type="button" name="6" className="itemK">6</button>
+          <button onClick={this.handleClick} type="button" name="-" className="itemL">-</button>
+          <button onClick={this.handleClick} type="button" name="1" className="itemM">1</button>
+          <button onClick={this.handleClick} type="button" name="2" className="itemN">2</button>
+          <button onClick={this.handleClick} type="button" name="3" className="itemO">3</button>
+          <button onClick={this.handleClick} type="button" name="+" className="itemP">+</button>
+          <button onClick={this.handleClick} type="button" name="0" className="itemQ">0</button>
+          <button onClick={this.handleClick} type="button" name="." className="itemR">.</button>
+          <button onClick={this.handleClick} type="button" name="=" className="itemS">=</button>
         </div>
       </div>
     );
